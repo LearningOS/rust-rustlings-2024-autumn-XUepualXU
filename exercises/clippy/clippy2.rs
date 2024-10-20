9d747309c2b6ba030d6f113f5f9c5cf369ad96f0
xuqianyi <xuqianyi@163.com>
2024-10-20 10:11:35 +0800

    update 2024-10-20

--- exercises/clippy/clippy2.rs
+++ exercises/clippy/clippy2.rs
@@ -4,12 +4,11 @@
 // hint.
 
 
-
 fn main() {
     let mut res = 42;
     let option = Some(12);
-    while let Some(x) = option {
-        res = x
+    if let Some(x) = option {
+        res += x;
     }
     println!("{}", res);
 }
